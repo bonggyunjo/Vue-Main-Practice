fa89bf4392ad194e68962af2e0aa67edf112a945
jbg <kjbg4565388@gmail.com>
2024-02-05 06:02:01 +0900

    vue date 수정

--- src/main.vue
+++ src/main.vue
@@ -29,42 +29,28 @@
    <div class="header-line"> 
     </div>
 
-   <div id="carouselExampleDark" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="4000">
-      <img  src="/image/banner1.jpg" class="d-block w-100" style=" background-color: lightgray;  height:610px;" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <p style="position: relative; top:20px;">웹 사이트를 방문해주셔서 감사합니다.</p>
-      </div>
-    </div>
-    <div class="carousel-item" data-bs-interval="4000">
-      <img class="d-block w-100" style=" background-color: lightgray;  height:610px;" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5 style="position: relative; top: -200px;">공모전</h5>
-        <p  style="position: relative; top:-200px;">content</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="/image/banner3.png" class="d-block w-100" style=" height:610px;" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <p  style="color:black; position: relative top:20px; font-weight:bolder;">웹 사이트를 방문해주셔서 감사합니다.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+    <div id="carouselExampleDark" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+  <button type="button" data-bs-target="#carouselExampleDark" v-for="(slide, index) in slides" :data-bs-slide-to="index" :class="{ 'active': index === 0 }" :aria-current="index === 0" :aria-label="'Slide ' + (index + 1)" :key="'indicator-' + index"></button>
 </div>
+<div class="carousel-inner">
+  <div v-for="(slide, index) in slides" :class="{ 'carousel-item': true, 'active': index === 0 }" :data-bs-interval="slide.interval" :key="'slide-' + index">
+        <img :src="slide.image" class="d-block w-100" :style="{ 'background-color': slide.backgroundColor, 'height':'610px' }" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5 v-if="slide.title" :style="{ 'position': 'relative', 'top': slide.titleTop }">{{ slide.title }}</h5>
+          <p :style="{ 'position': 'relative', 'top': slide.contentTop }">{{ slide.content }}</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
     <div class="card-view">
       <div class="card-list">
@@ -107,7 +93,30 @@ export default {
         { text: '일반 게시판 글쓰기', imgSrc: '/image/일반 글쓰기.png', class: 'write-image' },
         { text: '중고 게시판 글쓰기', imgSrc: '/image/중고 글쓰기.png', class: 'write-image' },
     ],
-    menuLinks: ['#', '#', '#','#','#']
+    menuLinks: ['#', '#', '#','#','#'],
+    slides: [
+        {
+          image: '/image/banner1.jpg',
+          backgroundColor: 'lightgray',
+          interval: 4000,
+          content: '웹 사이트를 방문해주셔서 감사합니다.',
+          contentTop: '20px'
+        },
+        {
+          backgroundColor: 'lightgray',
+          interval: 4000,
+          title: '공모전',
+          titleTop: '-200px',
+          content: 'content',
+          contentTop: '-200px'
+        },
+        {
+          image: '/image/banner3.png',
+          interval: 4000,
+          content: '웹 사이트를 방문해주셔서 감사합니다.',
+          contentTop: '20px'
+        }
+      ]
     };
   },
 
